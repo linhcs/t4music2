9f791117044bcd440d22823769ff7f762b14ef34
linhcs <linhcs@users.noreply.github.com>
2025-03-12 23:52:16 -0500

    Created song gallery with placeholder images

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,9 @@
+import Gallery from "@/components/ui/gallery";
+
+export default function SongGallery(){
+    return(
+        <div>
+            <Gallery/>
+        </div>
+    )
+}
---
+++ src/components/ui/gallery.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+const Gallery = () => {
+    // using placeholder images to create gallery
+    
+    const images = [
+        // static example, change later to fetch images from database
+        {id: 1, src: "/song-placeholder.jpg"},
+        {id: 2, src: "/song-placeholder.jpg"},
+        {id: 3, src: "/song-placeholder.jpg"},
+        {id: 4, src: "/song-placeholder.jpg"},
+        {id: 5, src: "/song-placeholder.jpg"},
+        {id: 6, src: "/song-placeholder.jpg"},
+    ];
+
+    return(
+        // can use in-line styling or cva (class-variance authoritity) to optimize repitition
+        // grid with 3 columns, spacing of 12px, padding of 12px (each increment of 1 increases by 4px)
+        <div className= "grid grid-cols-3 gap-3 p-3">
+            {images.map((image) => (
+                <div key = {image.id} className="flex justify-center">
+                    <img src = {image.src} className="w-full h-auto"/>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Gallery;
