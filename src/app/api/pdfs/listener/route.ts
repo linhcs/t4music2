21dec5aefcf9ae0ab551c57cc504e1cca56a5edb
OGMBlake <mcblake@cougarnet.uh.edu>
2025-04-07 11:48:47 -0500

    Now workin on Listener
    started listner, have pfp stuff done

--- src/app/api/pdfs/listener/route.ts
+++ src/app/api/pdfs/listener/route.ts
@@ -1,30 +1,32 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from "@prisma/client";
-import { PDFDocument } from 'pdf-lib';
+import { PDFDocument, rgb } from 'pdf-lib';
 import fs from 'fs';
 import path from 'path';
 import * as fontkit from 'fontkit';
-import { drawFanHeaders, addFanDataToPage } from '@/lib/tableutils';
 
 const prisma = new PrismaClient();
+interface pfpresult {v: string;};
 
-interface follows {username: string; follow_at: Date; }
+function getCurrentDateFormatted(): string {
+  const date = new Date();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get month (1-12), and pad to ensure 2 digits
+  const year = date.getFullYear().toString().slice(-2); // Get last two digits of the year
 
-export async function GET(req: Request) {
+  return `${month}-${year}`;
+}
+
+export async function POST(req: Request) {
   try {
 
-    const { username } : { username : string }  = await req.json();
-
-    const artistid: number[] = await prisma.$queryRaw`SELECT user_id FROM users WHERE username = ${username};`;
-    const followers: follows[] = await prisma.$queryRaw`
-    SELECT u.username, f.follow_at
-    FROM follows AS f, users AS u 
-    WHERE u.user_id = user_id_b AND f.user_id_a = ${artistid[0]};`;
-  
-    // Create a new PDF document
+    const currentDate = getCurrentDateFormatted();
+    const { username, user_id } = await req.json();
+    console.log('username: ', username);
+    console.log('user_id: ', user_id);
+    const pfp: pfpresult[] = await prisma.$queryRaw`SELECT pfp as v FROM users WHERE user_id = ${user_id} `;
+    
+    //new PDF 
     const pdfDoc = await PDFDocument.create();
-
-    // Add a blank page to the PDF
     let page = pdfDoc.addPage([612, 792]); // letters size
 
     //runing ignore becasue it says its a problem but it works so idk
@@ -34,25 +36,65 @@ export async function GET(req: Request) {
     const comicSansPath = path.resolve('public/fonts/comic.ttf');
     const comicSansBytes = fs.readFileSync(comicSansPath);
     const font = await pdfDoc.embedFont(comicSansBytes);
+    //background
+    const imagepath = path.resolve('public/even lighter.jpg');
+    const imageBytes = fs.readFileSync(imagepath);
+    const image = await pdfDoc.embedJpg(imageBytes);
+    page.drawImage(image);
+
+    
+    let pfpimage;
+    //profile picture
+    if(pfp[0].v !== null){
+      const response = await fetch(pfp[0].v);
+      const pfpBytes = await response.arrayBuffer()
+      pfpimage = await pdfDoc.embedJpg(new Uint8Array(pfpBytes));
+    } else {
+
+      let pfppath = "public/default_pfp.jpg";
+      const pfpBytes = fs.readFileSync(pfppath);
+      pfpimage = await pdfDoc.embedJpg(pfpBytes);
+    };
+    
+    const gradientColors = [
+          rgb(0, 153/255, 255/255),
+          rgb(167/255, 0/255, 209/255),
+          rgb(252/255, 142/255, 246/255),
+          rgb(0, 0, 0),
+        ];
+
+    for (let i = 0; i < gradientColors.length; i++) {
+      page.drawRectangle({
+          x: 56 - (i * 2),
+          y: 586 + (i * 2),
+          width: 150,
+          height: 150,
+          borderWidth: 4,
+          borderColor: gradientColors[i],
+          borderOpacity: 0.55 + (i * .15),
+      });
+      if(i == 2){
+        page.drawImage(pfpimage, {x: 50, y: 592, width: 150, height: 150});
+      };
+    };
+
+    page.drawText(username, { x:240, y:710, font, size: 24, });
+    page.drawText(currentDate, { x: 540, y:752, size: 14, opacity:0.5});
 
 
-    let yPosition = 720;
 
-    yPosition = drawFanHeaders(page, yPosition, font);
 
-    for (let i = 0; i < followers.length; i++) {
-      const fan = followers[i];
 
-      if (yPosition < 72) { // If there's not enough space on the current page, add a new one
-        page = pdfDoc.addPage([612, 792]); // Add a new page
-        yPosition = 720; // Reset position for the new page
-        // Draw headers again on the new page
-        yPosition = drawFanHeaders(page, yPosition, font);
-      }
 
-      // Add user data to the page
-      yPosition = addFanDataToPage(page, fan, yPosition, font);
-    }
+
+
+
+
+
+
+
+
+
 
     // Serialize the document to bytes (Uint8Array)
     const pdfBytes = await pdfDoc.save();
