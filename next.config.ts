d99f90d44622c821b9bdf845eaf49ee40496e09c
bmbo123 <gregoryalan2005@gmail.com>
2025-04-10 16:07:25 -0500

    example thing fix

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    AZURE_STORAGE_CONNECTION_STRING: process.env.AZURE_STORAGE_CONNECTION_STRING,
+    AZURE_STORAGE_CONNECTION_STRING:
+      process.env.AZURE_STORAGE_CONNECTION_STRING,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'musiclibraryfiles.blob.core.windows.net',
+        protocol: "https",
+        hostname: "musiclibraryfiles.blob.core.windows.net",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
       },
     ],
-  }
+    domains: ["example.com"],
+  },
 };
 
 export default nextConfig;
